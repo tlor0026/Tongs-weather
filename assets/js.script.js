5b8ad0f5535d5eb1791fc02db90b029c6232f258
tlor0026 <tlor0026@gmail.com>
2022-06-04 16:10:33 -0500

    initial commit

---
+++ assets/js.script.js
@@ -0,0 +1,11 @@
+var requestUrl= "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+
+var citySearch =$("#searchNow").on("click", function() {
+    console.log("this is working")
+    console.log('yo wahts up');
+}); 
+
+var pastCity =$("#onPast").on("click", function() {
+    console.log("this is working")
+    console.log('yo wahts up');
+}); 
